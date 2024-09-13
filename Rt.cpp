df09e1c7447e91bb29a816d39327564b21823480
yello <yellowbear360@gmail.com>
2024-09-13 21:33:46 +0200

    Fixes

--- Rt.cpp
+++ Rt.cpp
@@ -1,6 +1,17 @@
 #include "Rt.h"
+#include <random>
 
 double degreesToRadians(double degrees)
 {
     return degrees * pi / 180.0;
+}
+
+double randomDouble()
+{
+    return std::mt19937(std::random_device()())() / (std::mt19937::max() + 1.0);
+}
+
+double randomDouble(double min, double max)
+{
+    return min + ((max - min) * randomDouble());
 }
--- Rt.h
+++ Rt.h
@@ -6,4 +6,8 @@ const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 double degreesToRadians(double degrees);
+
+double randomDouble();
+
+double randomDouble(double min, double max);
 #endif//INC_3DRENDERER_RT_H
